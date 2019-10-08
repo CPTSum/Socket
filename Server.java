8664b6297edb934b82aa5a351d6fe068bcf97489
CPTSum <mpumelelomuy@gmail.com>
2019-10-08 12:22:27 +0200

    Server defined listening in Port 5000

--- Server.java
+++ Server.java
@@ -8,6 +8,37 @@ public class Server
     private ServerSocket server = null;
     private DataInputStream in = null;
 
+    public Server(int port)
+    {
+        try{
+            server = new ServerSocket(port);
+            System.out.println("Server has started");
+
+            System.out.println("Wait for client to connect ...");
+
+            socket = server.accept();
+            System.out.println("Client connection accepted");
+
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream())); //message from client
+            String line = '';
+            //listen
+            while(!line.equals("Done with connection"))
+            {
+                try{
+                    line = in.readUTF();
+                    System.out.println(line)}
+            }
+                catch(IOException i)
+                { System.out.println(i);
+                }
+            }
+            socket.close(); //closing the connection
+            in.close();
+            catch(IOException i)
+            {
+            System.out.println(i);
+            }
+    }
 }
 
     public static void main(String args[])
